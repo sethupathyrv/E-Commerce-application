134b384ae274221cbc312bb75439faa7401a43e7
SandeepTadepalli1 <tadepalli.sandeep@iiitb.org>
2018-03-13 21:04:57 +0530

    Cart & Order database

--- src/main/database.sql
+++ src/main/database.sql
@@ -1,45 +1,38 @@
+create table Cart
+(
+  id int auto_increment
+    primary key,
+  itemId int null,
+  userId int null,
+  quantity int null,
+  constraint Cart_id_uindex
+  unique (id)
+)
+  engine=InnoDB
+;
+
 create table Categories
 (
-    id int auto_increment
-        primary key,
-    name varchar(255) null,
-    displayName varchar(255) null,
-    isEnabled tinyint(1) default '1' null,
-    constraint Category_id_uindex
-    unique (id)
+  id int auto_increment
+    primary key,
+  name varchar(255) null,
+  displayName varchar(255) null,
+  isEnabled tinyint(1) default '1' null,
+  constraint Category_id_uindex
+  unique (id)
 )
-    engine=InnoDB
+  engine=InnoDB
 ;
 
-create table Users
+create table ItemDetails
 (
-    id int auto_increment
-        primary key,
-    userName varchar(255) not null,
-    emailId varchar(255) not null,
-    password varchar(255) not null,
-    isEnabled tinyint(1) default '1' not null,
-    constraint Users_id_uindex
-    unique (id),
-    constraint users_emailId_uindex
-    unique (emailId)
+  id int auto_increment
+    primary key,
+  itemId int not null,
+  `key` varchar(255) null,
+  value varchar(255) null
 )
-    engine=InnoDB
-;
-
-CREATE TABLE Sellers
-(
-    id int auto_increment PRIMARY KEY ,
-    userName VARCHAR(255) not null,
-    emailId VARCHAR(255) not null,
-    password VARCHAR(255) not NULL,
-    isEnabled TINYINT(1) DEFAULT '1' null,
-    CONSTRAINT Sellers_id_uindex
-    UNIQUE (id),
-    CONSTRAINT Sellers_emailId_uindex
-    UNIQUE (emailId)
-)
-    ENGINE =InnoDB
+  engine=InnoDB
 ;
 
 create table Items
@@ -47,7 +40,7 @@ create table Items
   id int auto_increment
     primary key,
   name varchar(255) not null,
-  itemPrice float not null,
+  price float not null,
   description varchar(1000) null,
   sellerId int null,
   offerId int null,
@@ -57,8 +50,81 @@ create table Items
   height float null,
   width float null,
   quantity int null,
+  isEnabled tinyint(1) default '1' not null,
   constraint Items_id_uindex
   unique (id)
+)
+  engine=InnoDB
+;
+
+create table Offers
+(
+  id int auto_increment
+    primary key,
+  offerType int null,
+  discountPercentage float default '-1' null,
+  price int default '-1' null,
+  constraint Offers_id_uindex
+  unique (id)
+)
+  engine=InnoDB
+;
+
+create table OrderItems
+(
+  id int auto_increment
+    primary key,
+  orderId int null,
+  itemId int null,
+  itemPrice int null,
+  quantity int null
+)
+  engine=InnoDB
+;
+
+create table Orders
+(
+  userId int not null,
+  itemsSubTotal int null,
+  shippingCharges int null,
+  deliveryAddressId int null,
+  orderStatus int null,
+  id int auto_increment
+    primary key,
+  constraint Orders_id_uindex
+  unique (id)
+)
+  engine=InnoDB
+;
+
+create table Sellers
+(
+  id int auto_increment
+    primary key,
+  userName varchar(255) not null,
+  emailId varchar(255) not null,
+  password varchar(255) not null,
+  isEnabled tinyint(1) default '1' null,
+  constraint Sellers_id_uindex
+  unique (id),
+  constraint Sellers_emailId_uindex
+  unique (emailId)
+)
+  engine=InnoDB
+;
+
+create table Users
+(
+  id int auto_increment
+    primary key,
+  userName varchar(255) not null,
+  emailId varchar(255) not null,
+  password varchar(255) not null,
+  isEnabled tinyint(1) default '1' not null,
+  constraint Users_id_uindex
+  unique (id),
+  constraint users_emailId_uindex
+  unique (emailId)
 )
   engine=InnoDB
 ;
